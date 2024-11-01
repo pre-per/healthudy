b7276809c22f6bb04bcce64947d4b62e9037ae1f
pre-per <snowha9557@korea.ac.kr>
2024-11-02 00:00:00 +0900

    color

---
+++ lib/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xFFFE2E64);
+final MAIN_WHITE_COLOR = Colors.white;
+final MAIN_GREY_COLOR = Colors.grey[400]!;
+final TEXT_BLACK_COLOR = Colors.black;
